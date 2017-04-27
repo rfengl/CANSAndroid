977eb641d81f8663723c5fb0dda23d07e888e0e8
mattan <tancheeleong82@gmail.com>
2017-04-27 20:46:14 +0800

    Monitoring

--- app/src/main/java/my/com/cans/cansandroid/activities/WebActivity.java
+++ app/src/main/java/my/com/cans/cansandroid/activities/WebActivity.java
@@ -1,13 +1,18 @@
 package my.com.cans.cansandroid.activities;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+
+import java.io.File;
 
 import my.com.cans.cansandroid.R;
 
@@ -59,6 +64,22 @@ public class WebActivity extends BaseActivity {
 
         reload();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.close, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_close) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void reload() {
